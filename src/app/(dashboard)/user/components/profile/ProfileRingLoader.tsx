cb615b01755a4b889fba89e2fd5dde95095e3cf7
zenwk <alineumsoft@gmail.com>
2025-12-11 14:01:51 -0500

    [refactor][naming | estrucut][V03]: reestructuración del proyecto con ba…

--- src/app/(dashboard)/user/components/profile/ProfileRingLoader.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { RingLoader } from 'react-spinners';
-
-const ProfileRingLoader = () => {
-    return <RingLoader color="#000000" size={17} speedMultiplier={1.5} />;
-};
-
-export default ProfileRingLoader;
