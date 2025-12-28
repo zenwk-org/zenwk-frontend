8d34422b0217efe0ddf79322e930fb82ea792edf
zenwk <alineumsoft@gmail.com>
2025-12-28 17:52:38 -0500

    [refactor][ambiente]: validación despliegue en ambiente test.
    [calegria]

--- src/components/shared/common/WelcomeSection.tsx
+++ src/components/shared/common/WelcomeSection.tsx
@@ -18,7 +18,7 @@ const WelcomeSection = () => {
                 icon: <CalendarDays className={themeIndex} size={30} />,
                 title: 'Planifica sin esfuerzo',
                 description:
-                    'Organiza tus actividades diarias y mantén control total de tus metas en un solo lugar.',
+                    '(Ambiente desarrollo perras!) Organiza tus actividades diarias y mantén control total de tus metas en un solo lugar.',
                 color: 'bg-sky-50',
             },
             {
